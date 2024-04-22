468237ea07fe464ac89d020033d103e0ad562cfd
renzxgrayGit <renzocabajar9@gmail.com>
2024-04-22 17:00:29 +0800

    CRUD

---
+++ src/Data.jsx
@@ -0,0 +1,17 @@
+export const userList = [
+    {
+        name: "renz cabajar",
+        email: "renzcabajar@gmail.com",
+        id: 1
+    },
+    {
+        name: "amy chris",
+        email: "amychris@gmail.com",
+        id: 2
+    },
+    {
+        name: "zoe cruz",
+        email: "zoecruz@gmail.com",
+        id: 3
+    }
+]
